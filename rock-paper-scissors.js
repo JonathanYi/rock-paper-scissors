1d467b148acac46a33e5a1a27e36909ed33d569a
Jonathan Yi <jonathanyi81@gmail.com>
2022-06-30 01:32:51 -0400

    Add function gameRound()
    gameRound takes two parameters
    player choice and computer choice
    case is ignored
    returns win results based on rock paper scissor rules

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,20 @@
 function computerPlay() {
     const rps = ['Rock','Paper','Scissors'];
-    return rps[Math.floor(Math.random()*3)]
+    return rps[Math.floor(Math.random()*3)];
+}
+
+function gameRound(playerSelection, computerSelection) {
+    let ps = playerSelection.toLowerCase();
+    let cs = computerSelection.toLowerCase();
+    if ((ps === 'rock' && cs === 'scissors') ||
+        (ps === 'paper' && cs === 'rock') ||
+        (ps === 'scissors' && cs === 'paper')) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (ps === cs) {
+        return `Tie! ${playerSelection} and ${computerSelection}`;
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
 }
